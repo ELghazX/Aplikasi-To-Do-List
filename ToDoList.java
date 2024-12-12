eac2173943a309b7d2dc4e6902dc0c744320f9e9
ELghazX <2309106041@student.unmul.ac.id>
2024-12-12 16:10:11 +0800

    feat(todolist): add delToDo method

--- ToDoList.java
+++ ToDoList.java
@@ -1,71 +1,84 @@
 public class ToDoList {
-    // Menentukan view
-    // view show
-    // view add
-    // view delete
-    public static String[] model = new String[10];
 
-    public static void main(String[] args) {
-        testAddToDo();
+  // Menentukan view
+  // view show
+  // view add
+  // view delete
+  public static String[] model = new String[10];
+
+  public static void main(String[] args) {
+    testAddToDo();
+  }
+
+  // menampilkan data
+  public static void getToDo() {
+    for (var i = 0; i < model.length; i++) {
+      var todo = model[i];
+      var no = i + 1;
+
+      if (todo != null) {
+        System.out.println(no + ". " + todo);
+      }
     }
+  }
 
-    // menampilkan data
-    public static void getToDo() {
-        for (var i = 0; i < model.length; i++) {
-            var todo = model[i];
-            var no = i + 1;
+  public static void testGetToDo() {
+    model[0] = "item 1";
+    model[1] = "item 2";
+    getToDo();
+  }
 
-            if (todo != null) {
-                System.out.println(no + ". " + todo);
-            }
-        }
+  // add data
+  public static void addToDo(String todo) {
+    for (var i = 0; i < model.length; i++) {
+      if (model[i] == null) {
+        model[i] = todo;
+        break;
+      }
     }
-
-    public static void testGetToDo() {
-        model[0] = "item 1";
-        model[1] = "item 2";
-        getToDo();
-    }
-
-    // add data
-    public static void addToDo(String todo) {
-        for (var i = 0; i < model.length; i++) {
-            if (model[i] == null) {
-                model[i] = todo;
-                break;
-            }
-        }
-        var isFull = true;
-        for (var i = 0; i < model.length; i++) {
-            if (model[i] == null) {
-                isFull = false;
-                break;
-            }
-
-        }
-        // jika data penuh di array, kalikan 2 yang baru
-        if (isFull) {
-            var temp = model;
-            model = new String[model.length * 2];
-            for (var i = 0; i < temp.length; i++) {
-                model[i] = temp[i];
-            }
-        }
-    }
-
-    public static void testAddToDo() {
-        for (var i = 0; i < 25; i++) {
-            addToDo("todo ke - " + i);
-        }
-        getToDo();
+    var isFull = true;
+    for (var i = 0; i < model.length; i++) {
+      if (model[i] == null) {
+        isFull = false;
+        break;
+      }
 
     }
-
-    public static void viewAddToDo() {
-
+    // jika data penuh di array, kalikan 2 yang baru
+    if (isFull) {
+      var temp = model;
+      model = new String[model.length * 2];
+      for (var i = 0; i < temp.length; i++) {
+        model[i] = temp[i];
+      }
     }
+  }
 
-    public static void viewDelToDo() {
-
+  public static void testAddToDo() {
+    for (var i = 0; i < 25; i++) {
+      addToDo("todo ke - " + i);
     }
+    getToDo();
+
+  }
+
+  public static boolean delToDo(Integer number) {
+    if ((number - 1) >= model.length) {
+      return false;
+    } else if ((model[number - 1]) == null) {
+
+      return false;
+    } else {
+      model[number - 1] = null;
+      return true;
+    }
+  }
+
+  public static void viewAddToDo() {
+
+  }
+
+  public static void viewDelToDo() {
+
+  }
 }
